1e9a989659f70d62f3dd99baf8f4ffd21a783489
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-21 17:20:57 +0200

    Rename components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieRecommendationComponent } from './Components/movie-recommendation/movie-recommendation.component';
-import { MovieListComponent } from './Components/movie-list/movie-list.component';
-import { MovieShowComponent } from './Components/movie-show/movie-show.component';
+import { MovieShowComponent } from './components/movie-show/movie-show.component';
+import { MovieRecommendationComponent } from './components/movie-recommendation/movie-recommendation.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
