e532d3d3febe24e234bb0e3b599d092a683db9ff
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-22 00:01:31 +0200

    Add components routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieShowComponent } from './components/movie-show/movie-show.component';
 
 const routes: Routes = [
-  {path:'',component:MovieListComponent},
+  {path: '', component: MovieListComponent},
+  {path: 'movies', component: MovieListComponent},
+  {path: 'movies/:id', component: MovieShowComponent},
 ];
 
 @NgModule({
