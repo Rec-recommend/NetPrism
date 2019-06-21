12e7ac312ea3b6a96035aedbe86046d9318feb9e
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-22 00:02:58 +0200

    Add services to appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { MovieShowComponent } from './components/movie-show/movie-show.component';
 import { MovieRecommendationComponent } from './components/movie-recommendation/movie-recommendation.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieService } from './services/movie.service';
+import { GoogleService } from './services/google.service';
+import { RecommendationService } from './services/recommendation.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,7 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MovieService, GoogleService, RecommendationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
