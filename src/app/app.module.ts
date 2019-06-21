e86433de5d5d904349e2c1e0dec67dc4414faa81
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-21 16:55:13 +0200

    Add components list/show/recommendation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieRecommendationComponent } from './Components/movie-recommendation/movie-recommendation.component';
+import { MovieListComponent } from './Components/movie-list/movie-list.component';
+import { MovieShowComponent } from './Components/movie-show/movie-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieShowComponent,
+    MovieRecommendationComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
