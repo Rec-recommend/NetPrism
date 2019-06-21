9847a5eb3f1cd4c23c0150f43fafe50af4cd88a2
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-22 00:03:23 +0200

    Add movie recommendation component

--- src/app/components/movie-recommendation/movie-recommendation.component.ts
+++ src/app/components/movie-recommendation/movie-recommendation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RecommendationService } from 'src/app/services/recommendation.service';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-movie-recommendation',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-recommendation.component.scss']
 })
 export class MovieRecommendationComponent implements OnInit {
+  public similarMovies: Movie[] = [];
+  constructor(recService: RecommendationService) {
 
-  constructor() { }
+    recService.fetchSimilarMovies(1).subscribe(movies => this.similarMovies = movies);
+  }
 
   ngOnInit() {
   }
