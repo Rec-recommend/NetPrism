247e5b29e84e40b58d37320c2d53514aa7bfd984
a-tarek <a.tarek.elsayed@gmail.com>
2019-06-23 16:34:47 +0200

    Show movie title and genre in movie-show

--- src/app/components/movie-show/movie-show.component.ts
+++ src/app/components/movie-show/movie-show.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/models/movie';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-show',
@@ -8,9 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovieShowComponent implements OnInit {
   public id: number;
+  public movie:Movie;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private movieService:MovieService) {
     this.id = this.route.params['value'].id;
+    this.movie = this.movieService.findById(this.id)
    }
 
   ngOnInit() {
