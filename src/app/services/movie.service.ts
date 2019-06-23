358bd8472c404f467d8700b20f31ae940842b67d
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-06-23 17:30:29 +0200

    Extend find method  in movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -17,13 +17,13 @@ export class MovieService {
   public findById(id: number):Movie{
     return moviesHash[id];
   }
-  public find(ids: number[]) {
+  public find(ids: number[], max: number = 6) {
     let movies: Movie[] = [];
     try {
       ids.forEach((id)=> movies.push(moviesHash[id]))
     } catch(e){
       console.log(e)
     }
-    return movies
+    return movies.slice(0, max)
   }
 }
